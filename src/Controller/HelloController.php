a7196e5bba6180ae359053d550921cfed2d4625c
Nourchene <nourchene.laroussi@esprit.tn>
2024-06-07 21:11:28 +0200

    Formation Terminé

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\DTO\ContactDTO;
 use App\Entity\Post;
 use App\Entity\Category;
+use App\RemoteApiService;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\Entity;
@@ -20,70 +21,74 @@ class HelloController extends AbstractController
 {
     #[Route('/hello/{name}', name: 'app_hello')]
     // #[IsGranted('ROLE_USER')]
-    public function index(Request $request,String $name,EntityManagerInterface $manager,PostRepository $repository ): Response
+    // public function index(Request $request,String $name,EntityManagerInterface $manager,PostRepository $repository ): Response
+    public function index(RemoteApiService $service ): Response
     {
-        // $posts=new Post();
-        // //EntityManagerInterface utilisée pour interagir avec les entités et la base de données dans Symfony
-        // $posts->setCreatedAt(new \DateTimeImmutable());
-        // $posts->setUpdatedAt(new \DateTimeImmutable());
-        // $posts->setTitle('Mon premier Article');
-        // $posts->setSlug('mon-premier-article');
-        // // $post->setContent('Mon contenu est ecrit avec une idée en tete');
-        // // // Enregistre l'objet $post dans la base de données
-        // // $manager->persist($post);
-        // // $manager->flush(); //insert dans la base de donnees
-        // // // dd($manager);
-        // //recuperer tout les donnees a partir de la base donnees :select * dans un tableau
-        // // $posts=$manager->getRepository(Post::class)->findAll();
-        // // $posts = $manager->getRepository(Post::class)->findBy(['slug' => 'mon-premier-article']);//chercher par slug (une valeur specifique)
-        // // $posts = $manager->getRepository(Post::class)->findOrFailBySlug('mon-premier-article');//chercher par slug methode que j'ai defini dans postRepisotory
-        // //ou 
-        // // $posts = $repository->findOrFailBySlug('mon-premier-article');//chercher par slug methode que j'ai defini dans postRepisotory //par la declaration d'un repository
-        // // $posts->setTitle('mon nouveau titre');
-        // // $manager->remove($posts);//suprimer une entitee
+    //     // $posts=new Post();
+    //     // //EntityManagerInterface utilisée pour interagir avec les entités et la base de données dans Symfony
+    //     // $posts->setCreatedAt(new \DateTimeImmutable());
+    //     // $posts->setUpdatedAt(new \DateTimeImmutable());
+    //     // $posts->setTitle('Mon premier Article');
+    //     // $posts->setSlug('mon-premier-article');
+    //     // // $post->setContent('Mon contenu est ecrit avec une idée en tete');
+    //     // // // Enregistre l'objet $post dans la base de données
+    //     // // $manager->persist($post);
+    //     // // $manager->flush(); //insert dans la base de donnees
+    //     // // // dd($manager);
+    //     // //recuperer tout les donnees a partir de la base donnees :select * dans un tableau
+    //     // // $posts=$manager->getRepository(Post::class)->findAll();
+    //     // // $posts = $manager->getRepository(Post::class)->findBy(['slug' => 'mon-premier-article']);//chercher par slug (une valeur specifique)
+    //     // // $posts = $manager->getRepository(Post::class)->findOrFailBySlug('mon-premier-article');//chercher par slug methode que j'ai defini dans postRepisotory
+    //     // //ou 
+    //     // // $posts = $repository->findOrFailBySlug('mon-premier-article');//chercher par slug methode que j'ai defini dans postRepisotory //par la declaration d'un repository
+    //     // // $posts->setTitle('mon nouveau titre');
+    //     // // $manager->remove($posts);//suprimer une entitee
         
-        // $category1=new Category();
-        // $category2=new Category();
-        // $category3=new Category();
-        // $category1->setName('Categorie #1 ');
-        // $category2->setName('Categorie #2 ');
-        // $category3->setName('Categorie #3 ');
+    //     // $category1=new Category();
+    //     // $category2=new Category();
+    //     // $category3=new Category();
+    //     // $category1->setName('Categorie #1 ');
+    //     // $category2->setName('Categorie #2 ');
+    //     // $category3->setName('Categorie #3 ');
 
-        // $manager->persist($category1);
-        // $manager->persist($category2);
-        // $manager->persist($category3);
-        // $posts = $repository->findOrFailBySlug('mon-premier-article');//chercher par slug methode que j'ai defini dans postRepisotory //par la declaration d'un repository
-        // $posts->setCategory($category1);
-        // $manager->flush();
+    //     // $manager->persist($category1);
+    //     // $manager->persist($category2);
+    //     // $manager->persist($category3);
+    //     // $posts = $repository->findOrFailBySlug('mon-premier-article');//chercher par slug methode que j'ai defini dans postRepisotory //par la declaration d'un repository
+    //     // $posts->setCategory($category1);
+    //     // $manager->flush();
 
-        //ouuuuuuuuuuuuuuuuuuuuuu
+    //     //ouuuuuuuuuuuuuuuuuuuuuu
 
-        // $posts = $repository->findOrFailBySlug('mon-premier-article');//chercher par slug methode que j'ai defini dans postRepisotory //par la declaration d'un repository
-        // dd($posts->getCategory()->getName(),$posts->getCategory());
-        // $posts = $repository->findAllWithCategory();//chercher par slug methode que j'ai defini dans postRepisotory //par la declaration d'un repository
+    //     // $posts = $repository->findOrFailBySlug('mon-premier-article');//chercher par slug methode que j'ai defini dans postRepisotory //par la declaration d'un repository
+    //     // dd($posts->getCategory()->getName(),$posts->getCategory());
+    //     // $posts = $repository->findAllWithCategory();//chercher par slug methode que j'ai defini dans postRepisotory //par la declaration d'un repository
         
-    $post=$repository->find(1);
-     $this->denyAccessUnlessGranted(PostVoter::VIEW,$post);
+    // $post=$repository->find(1);
+    //  $this->denyAccessUnlessGranted(PostVoter::VIEW,$post);
         
-        // dd($posts);//afficherrr
-        //la creation d'un objet vide de type ContactDTO
-        $data=new ContactDTO();
-        //creation d'un formulaire de la class contact
-        $form = $this->createForm(ContactForumType::class,$data);
-        $form->handleRequest($request);
-        // make my formulaire gere la requette 
-        if($form->isSubmitted() && $form->isValid())
-        {
-            // recuperer les donner de formulaire
-            dd($form->getData(),$data);
-        }
+    //     // dd($posts);//afficherrr
+    //     //la creation d'un objet vide de type ContactDTO
+    //     $data=new ContactDTO();
+    //     //creation d'un formulaire de la class contact
+    //     $form = $this->createForm(ContactForumType::class,$data);
+    //     $form->handleRequest($request);
+    //     // make my formulaire gere la requette 
+    //     if($form->isSubmitted() && $form->isValid())
+    //     {
+    //         // recuperer les donner de formulaire
+    //         dd($form->getData(),$data);
+    //     }
       
-        return $this->render('hello/index.html.twig',[
-            'name'=> $name,
-            'posts' => [], 
-            'form'=> $form
+    //     return $this->render('hello/index.html.twig',[
+    //         'name'=> $name,
+    //         'posts' => [], 
+    //         'form'=> $form
 
 
-    ]);
+    // ]);
+    dd($service);
+
+    
     } 
 }
--- src/RemoteApiService.php
+++ src/RemoteApiService.php
@@ -2,11 +2,11 @@
 
 namespace App;
 
-
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class RemoteApiService 
 {
-   public function __construct(public string $api)
+   public function __construct(#[Autowire('%demo_api_key')]public string $api)
    {
 
    }
